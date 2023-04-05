bbdf9ea477427d83f0b77d7a6410af4696401f0c
Kerimkhanov Kurban <kurban.kerimhanov@gmail.com>
2023-04-06 01:54:58 +0300

    Update conf.py

--- src/conf.py
+++ src/conf.py
@@ -2,4 +2,4 @@ import os
 
 PATH_TO_BSON = os.path.dirname(__file__) + "/dump/sampleDB/sample_collection.bson"
 DB_NAME = "mymongo"
-BOT_TOKEN = "6217639496:AAG2CkyHyo6v5SEfRzjpUEreIMxvfL27xzQ"
+BOT_TOKEN = "YOUR TOKEN"
